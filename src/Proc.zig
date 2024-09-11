eaa08f4dbfeeab2e4b095e9816cbbbecedac6603
Mitch Kennedy <mitchk@runbox.com>
2024-09-11 21:23:34 +1000

    work

--- src/Proc.zig
+++ src/Proc.zig
@@ -1,16 +1,26 @@
 const std = @import("std");
 
 pub const LaunchOpts = struct {
-    prog: []const u8,
-    args: []const [:0]const u8,
-    stdin: ?std.posix.fd_t = null,
-    stdout: ?std.posix.fd_t = null,
-    stderr: ?std.posix.fd_t = null,
+    progs: []const LaunchProg,
+    in: ?std.posix.fd_t,
+    out: ?std.posix.fd_t,
+    err: ?union(enum) {
+        per_proc: []const std.posix.fd_t,
+        one: std.posix.fd_t,
+    },
 };
 
-pub const LaunchResult = struct {};
+//fn setup_fds(opts: LaunchOpts,
 
-pub fn launch(
-    opts: LaunchOpts,
-    alloc: std.mem.Allocator,
-) !LaunchResult {}
+pub fn launch(opts: LaunchOpts, alloc: std.mem.Allocator) !LaunchReult {
+    if (opts.err) |e| {
+        switch (e) {
+            .per_proc => |p| std.debug.assert(p.len == opts.progs.len),
+            else => {},
+        }
+    }
+
+    for (opts.progs, 0..) |p, idx| {
+        try setup_fds(opts, idx);
+    }
+}
